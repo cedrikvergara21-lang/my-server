eac737b6e1546336aafcd7ab062de8326087379b
cedrikvergara21-lang <cedrikvergara21@gmail.com>
2026-03-23 10:30:47 +0800

    fix middleware order

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose  = require('mongoose');
 const bcrypt    = require('bcryptjs');
 const jwt       = require('jsonwebtoken');
 const app = express();
+app.use(express.json());
 app.use(express.static('.'));
 
 // ① Connect to MongoDB
